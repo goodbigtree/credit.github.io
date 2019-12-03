e207be051154e2ef2c5c8811783ebf0f4921e2d2
yangfeifei <goodbigtree@163.com>
2019-12-03 09:28:27 +0800

    doc modify

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   base: '/credit/',
   title: 'credit',
-  description: 'Vuepress blog demo',
+  description: 'Vuepress credit doc',
   head: [
     ['link', { rel: 'icon', href: '/vue-logo.png' }]
   ],
@@ -9,14 +9,14 @@ module.exports = {
   	// 你的GitHub仓库
     repo: 'https://github.com/goodbigtree/credit.github.io.git',
     // 自定义仓库链接文字。
-    repoLabel: 'My GitHub',
+    repoLabel: 'credit doc',
   	nav: [
-  		{ text: 'Home', link: '/' },
-  		{ text: 'FirstBlog', link: '/blog/FirstBlog.md' }
+  		{ text: '首页', link: '/' },
+  		{ text: '云信用', link: '/credit/Credit.md' }
   	],
   	sidebar: [
       ['/', '首页'],
-      ['/blog/FirstBlog.md', '我的第一篇博客']
+      ['/credit/Credit.md', '架构说明']
     ]
   }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,10 +1,10 @@
 ---
 home: true
 heroImage: /vue-logo.png
-heroText: blog-demo
+heroText: credit-dos
 tagline: 博客示例
 actionText: 快速上手 →
-actionLink: /blog/FirstBlog.md
+actionLink: /credit/Credit.md
 features:
 - title: 简洁至上
   details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
