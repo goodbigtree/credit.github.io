206531216b01a54179311605e58e7323a1f76cfa
yangfeifei <goodbigtree@163.com>
2019-12-03 08:55:09 +0800

    commint message

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:olewaHHH/blog-demo.git master:gh-pages
+git push -f https://github.com/goodbigtree/credit.github.io.git master:credit.github.io
 
 cd -
